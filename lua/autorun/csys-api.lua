6fa4e8af3d1414d715bececf10acdad919dff33b
Shark_vil <sharkvil228@gmail.com>
2020-05-17 23:51:17 +0300

    Adding project files and comments

--- lua/autorun/csys-api.lua
+++ lua/autorun/csys-api.lua
@@ -1,16 +1,40 @@
+-- Global variables
 csys = csys or {};
 csys.api = csys.api or {};
 
+-- Main app directory
 local FrameworkDir = 'csys-api';
+-- Project lua files
 local LuaFiles = {
+    --[[ SERVER ]]--
+    -- Stores client data during the game on the server
+    'client/data',
+    -- Responsible for processing data regarding clothing
+    'client/meta/clothing',
+    'client/clothing/net/receiver',
+    -- Responsible for visualizing clothes on the client
+    'client/meta/visualizer',
+    'client/visualizer/net/receiver',
 
+    --[[ CLIENT ]]--
+    -- Stores client data during the game on the server
+    'server/data',
+    -- Responsible for processing data regarding clothing
+    'server/meta/clothing'
+    'server/clothing/net/receiver',
+    -- Responsible for visualizing clothes on the server
+    'server/meta/visualizer',
+    'server/visualizer/net/receiver',
 };
 
+--- Load the project script to the game.
+-- @param FilePath lua script file path
 local function ScriptLoader( FilePath )
 
     FilePath = string.lower( FilePath );
 
     if ( not file.Exists( FilePath, 'LUA' ) ) then
+        MsgN( 'Not found this lua file - ' .. FilePath );
         return;
     end;
 
@@ -48,6 +72,9 @@ MsgN( '---------- CSYS API ----------' );
 MsgN( '------------------------------' );
 MsgN( '-- Loading...' );
 
+--[[
+    The loop iterates over the table files and loads them into the game.
+--]]
 for i = 1, table.Count( LuaFiles ) do
     local LuaFilePath = FrameworkDir .. '/' .. LuaFiles[ i ] .. '.lua';
     ScriptLoader( LuaFilePath );
